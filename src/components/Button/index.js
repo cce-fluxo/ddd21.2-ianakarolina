8742b462f8b820175dd56b6653a576ab2f2f2f80
Iana Karolina <ianakarolina@poli.ufrj.br>
2022-02-23 19:09:54 -0300

    botão finalizado

---
+++ src/components/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {Container, ButtonText} from './styles'
+
+const Button = ({width,height,text,backgroundcolor,radius,margintop,border,onPress}) => {
+    return <Container 
+                width={width} 
+                height={height} 
+                backgroundcolor={backgroundcolor} 
+                radius={radius} 
+                margintop={margintop} 
+                border={border}
+                onPress={onPress}>
+        <ButtonText>{text}</ButtonText>
+    </Container>
+}
+
+export default Button;
