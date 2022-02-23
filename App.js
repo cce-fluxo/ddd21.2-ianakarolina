3b4e700476d2bbd792530982315cb312c64a0575
Iana Karolina <ianakarolina@poli.ufrj.br>
2022-02-23 19:04:54 -0300

    pastas estruturadas

---
+++ App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+
+
+export default function App() {
+  return (
+  
+  <StatusBar style="auto" backgroundColor='#fff' />
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
